5cfcc066e36f5f67394f71ab6837b3265718feb7
davidseoservice <davidseoservice1211@gmail.com>
2026-07-20 19:32:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-document.querySelectorAll('a').forEach(a=>{if(a.hostname&&a.hostname!==location.hostname)a.target='_blank'});
+document.querySelectorAll("a").forEach(function (link) {
+  if (link.hostname && link.hostname !== location.hostname) {
+    link.target = "_blank";
+  }
+});
+
+const connectButton = document.querySelector(".small");
+
+if (connectButton) {
+  connectButton.href = "https://www.facebook.com/Davvidd.seo";
+  connectButton.textContent = "Let's Connect ↗";
+  connectButton.target = "_blank";
+}
